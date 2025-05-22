4a0d549bd447320544507b189e7c8f097f5cb395
Ethan <78313955+Ethan-Myles@users.noreply.github.com>
2025-05-22 17:05:46 +0100

    404 page not found V4

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,7 +3,7 @@ function PageNotFound() {
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
       <h2>404 - Page Not Found</h2>
-      <p>Oops, this page doesn't currently exist</p>
+      <p>Oops, this page doesn't currently exist!</p>
     </div>
   );
 }
